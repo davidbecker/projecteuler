2bee18daef9108513c012625c316864db0fe03b0
David Becker <dasmau89@gmail.com>
2017-10-03 15:01:46 +0100

    problem 4 solved

--- ProjectEuler/projecteuler.c
+++ ProjectEuler/projecteuler.c
@@ -1,5 +1,3 @@
-#include "stdio.h"
-#include "stdint.h"
 #include "projecteuler.h"
 
 
@@ -10,11 +8,13 @@ int main()
 	return 0;
 }
 
-void euler1() {
+void euler1()
+{
 	int sum = 0;
 	for (int i = 0; i < 1000; i++)
 	{
-		if (i % 3 == 0 | i % 5 == 0) {
+		if (i % 3 == 0 | i % 5 == 0)
+		{
 			sum += i;
 		}
 	}
@@ -22,13 +22,15 @@ void euler1() {
 	getchar();
 }
 
-void euler2() {
+void euler2()
+{
 	int seqLast = 1, seqCurrent = 1, sum = 0;
 	while (seqCurrent < 4000000)
 	{
 		seqCurrent += seqLast;
 		seqLast = seqCurrent - seqLast;
-		if (seqCurrent % 2 == 0) {
+		if (seqCurrent % 2 == 0)
+		{
 			sum += seqCurrent;
 		}
 	}
@@ -36,14 +38,17 @@ void euler2() {
 	getchar();
 }
 
-void euler3() {
+void euler3()
+{
 	uintmax_t target = 600851475143;
 
 	int largestPrime = 0, quotient = 2;
 	while (target != 0 & quotient <= target)
 	{
-		if (target%quotient == 0) {
-			if (quotient > largestPrime) {
+		if (target%quotient == 0)
+		{
+			if (quotient > largestPrime)
+			{
 				largestPrime = quotient;
 			}
 			target /= quotient;
@@ -54,24 +59,69 @@ void euler3() {
 		}
 	}
 
-	printf("lagest prime: %d", largestPrime);
+	printf("largest prime: %d", largestPrime);
 	getchar();
 }
 
-void selectProblem() {
+void euler4()
+{
+	int result = 0, iTemp = 1001;
+	isPalindrome(&iTemp);
+	for (int i = 100; i < 1000; i++)
+	{
+		for (int j = 100; j < 1000; j++)
+		{
+			iTemp = i * j;
+			if (isPalindrome(&iTemp))
+			{
+				if (iTemp > result)
+				{
+					result = iTemp;
+				}
+			}
+		}
+	}
+	printf("result: %d", result);
+	getchar();
+}
+
+bool isPalindrome(int *input)
+{
+	if (*input <= 0)
+	{
+		return false;
+	}
+	char str[12];
+	snprintf(str, 12, "%d", *input);
+	int length = strlen(str);
+
+	for (int i = 0; i < length; i++)
+	{
+		if (str[i] != str[length-i-1])
+		{
+			return false;
+		}
+	}
+
+	return true;
+}
+
+void selectProblem()
+{
 	int selection;
 	printf("select problem between %d and %d\n", PROBLEM_MIN, PROBLEM_MAX);
 	scanf_s("%d", &selection);
-	
+
 	if (selection >= PROBLEM_MIN & selection <= PROBLEM_MAX) {
 		switch (selection)
 		{
 		case 1: euler1(); break;
 		case 2: euler2(); break;
 		case 3: euler3(); break;
+		case 4: euler4(); break;
 		default:
 			break;
 		}
 	}
-	
+
 }
--- ProjectEuler/projecteuler.h
+++ ProjectEuler/projecteuler.h
@@ -1,10 +1,17 @@
 #pragma once
+#include "stdio.h"
+#include "stdint.h"
+#include "stdbool.h"
+
 
 const int PROBLEM_MIN = 1;
-const int PROBLEM_MAX = 3;
+const int PROBLEM_MAX = 4;
 
 void selectProblem();
 
 void euler1();
 void euler2();
 void euler3();
+void euler4();
+
+bool isPalindrome(int*);
