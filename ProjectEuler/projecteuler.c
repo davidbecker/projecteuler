92dc94cc21527bbfe5c24277373d3ea4c2e7e92b
David Becker <dasmau89@gmail.com>
2017-10-03 13:10:54 +0100

    problem 2 solved

--- ProjectEuler/projecteuler.c
+++ ProjectEuler/projecteuler.c
@@ -1,9 +1,6 @@
 #include "stdio.h"
 #include "projecteuler.h"
 
-const int PROBLEM_MIN = 1;
-const int PROBLEM_MAX = 1;
-
 int main()
 {
 	selectProblem();
@@ -23,6 +20,20 @@ void euler1() {
 	getchar();
 }
 
+void euler2() {
+	int seqLast = 1, seqCurrent = 1, sum = 0;
+	while (seqCurrent < 4000000)
+	{
+		seqCurrent += seqLast;
+		seqLast = seqCurrent - seqLast;
+		if (seqCurrent % 2 == 0) {
+			sum += seqCurrent;
+		}
+	}
+	printf("sum: %d", sum);
+	getchar();
+}
+
 void selectProblem() {
 	int selection;
 	printf("select problem between %d and %d\n", PROBLEM_MIN, PROBLEM_MAX);
@@ -32,6 +43,7 @@ void selectProblem() {
 		switch (selection)
 		{
 		case 1: euler1(); break;
+		case 2: euler2(); break;
 		default:
 			break;
 		}
--- ProjectEuler/projecteuler.h
+++ ProjectEuler/projecteuler.h
@@ -1,5 +1,9 @@
 #pragma once
 
+const int PROBLEM_MIN = 1;
+const int PROBLEM_MAX = 2;
+
 void selectProblem();
 
 void euler1();
+void euler2();
